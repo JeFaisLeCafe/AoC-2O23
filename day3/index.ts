1962a581a8a46b86c52f79ed4f9e89559783a520
Pierre-Etienne Soury <pierretienne.soury@gmail.com>
2023-12-03 12:22:11 +0100

    day3 part1

---
+++ day3/index.ts
@@ -0,0 +1,133 @@
+// load input from input.txt
+
+import _ from "lodash";
+
+// const path = "day3/test.txt"; // path relative to project root
+const path = "day3/input.txt"; // path relative to project root
+const file = Bun.file(path);
+const text = await file.text();
+const input = text.split("\n");
+
+interface Part {
+  x: number;
+  y: number;
+  value: number;
+  isValid: boolean;
+  neighbors: Position[]; // list of positions of the neighbors; will then have to check if, in the neighbors, there is a symbol
+}
+
+interface Position {
+  x: number;
+  y: number;
+  symbol: string;
+}
+// Part 1
+const part1 = () => {
+  // 2 possible approach:
+  // - look for symbols, and compute the numbers around them
+  // - look for numbers and check if there is a symbol around them
+  // I think the second approach is easier
+
+  // first we need to find if one of the digit is adjacent to a symbol
+  // a symbol is anything not a digit and not a dot (.)
+  // adjacent means that a symbol is on the left, right, top, bottom or diagonal
+  // we can use a regex to find the symbols
+  const symbolsRegex = /[^\d.]/g;
+
+  // we need to build an array of Parts
+  // a Part is an object with :
+  // - an array of valid neighbors
+  // - a value
+  // let's build this array of Parts
+  const partialParts: Part[] = [];
+  // we need to loop through the input
+  for (let y = 0; y < input.length; y++) {
+    const line = input[y];
+    for (let x = 0; x < line.length; x++) {
+      const char = line[x];
+      // if the char is a digit, we need to check if there is a symbol around it
+      if (/\d/.test(char)) {
+        const part: Part = {
+          x,
+          y,
+          value: parseInt(char),
+          isValid: false,
+          neighbors: []
+        };
+        // add all the neighbors to the list of neighbors
+        const neighbors: Position[] = [];
+        for (let i = -1; i <= 1; i++) {
+          const y2 = y + i;
+          if (y2 < 0 || y2 >= input.length) continue;
+          const line2 = input[y2];
+          for (let j = -1; j <= 1; j++) {
+            const x2 = x + j;
+            if (x2 < 0 || x2 >= line2.length) continue;
+            const char2 = line2[x2];
+            const neighbor = { x: x2, y: y2, symbol: char2 };
+            if (symbolsRegex.test(char2)) {
+              part.isValid = true;
+            }
+            neighbors.push(neighbor);
+          }
+        }
+        part.neighbors = neighbors;
+        partialParts.push(part);
+      }
+    }
+  }
+
+  // we now have an array of parts
+  // we need to "merge" the parts that are adjacent on the same line
+
+  function mergePartialParts(part1: Part, part2: Part): Part {
+    // merge
+    const newPart = {
+      x: part2.x,
+      y: part1.y,
+      value: part1.value * 10 + part2.value,
+      isValid: part1.isValid || part2.isValid,
+      neighbors: _.uniqBy([...part1.neighbors, ...part2.neighbors], (n) => {
+        return n.x + "_" + n.y;
+      })
+    };
+    return newPart;
+  }
+
+  const parts = [];
+  // loop through the partial parts
+  for (let i = 0; i < partialParts.length; i++) {
+    const part = partialParts[i];
+    // if the part is already in the parts, we need to merge it
+    const lastPart = parts[parts.length - 1];
+    if (lastPart && lastPart.y === part.y && lastPart.x === part.x - 1) {
+      // we need to merge
+      const newPart = mergePartialParts(lastPart, part);
+      parts[parts.length - 1] = newPart;
+    } else {
+      // we need to add the part to the parts
+      parts.push(part);
+    }
+  }
+
+  const sum = parts.reduce((acc, part) => {
+    if (part.isValid) {
+      return acc + part.value;
+    }
+    return acc;
+  }, 0);
+
+  console.log(
+    "parts",
+    parts.map((p) => {
+      return {
+        value: p.value,
+        isValid: p.isValid
+      };
+    })
+  );
+
+  console.log("part1", sum);
+};
+
+part1();
